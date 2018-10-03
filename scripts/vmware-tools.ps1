dec28a26c1fec6e8d05181654457c26732106cbb
eaksel <emre.aksel@ymail.com>
2018-10-03 11:59:25 +0200

    Updated to vmware tools v10.3.2

--- scripts/vmware-tools.ps1
+++ scripts/vmware-tools.ps1
@@ -1,5 +1,5 @@
-$package = "VMware-tools-10.2.1-8267844-x86_64.exe"
-$url = "https://packages.vmware.com/tools/releases/10.2.1/windows/x64/$package"
+$package = "VMware-tools-10.3.2-9925305-x86_64.exe"
+$url = "https://packages.vmware.com/tools/releases/10.3.2/windows/x64/$package"
 
 Write-Output "***** Downloading VMWare tools"
 Invoke-WebRequest $url -UseBasicParsing -OutFile "C:\Windows\Temp\$package"
@@ -10,5 +10,5 @@ $parameters = '/S /v "/qn REBOOT=R ADDLOCAL=ALL"'
 Write-Output "***** Installing VMWare Guest Tools"
 Start-Process $exe $parameters -Wait
 
-Write-Output "***** Deleting $isopath"
+Write-Output "***** Deleting $exe"
 Remove-Item $exe
