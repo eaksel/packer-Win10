24472bfe34ddeeae8eb87b08d25216d71051d939
eaksel <emre.aksel@ymail.com>
2019-10-28 16:33:35 +0100

    Updated to vmware tools v11.0.1

--- scripts/vmware-tools.ps1
+++ scripts/vmware-tools.ps1
@@ -1,5 +1,5 @@
-$package = "VMware-tools-10.3.2-9925305-x86_64.exe"
-$url = "https://packages.vmware.com/tools/releases/10.3.2/windows/x64/$package"
+$package = "VMware-tools-11.0.1-14773994-x86_64.exe"
+$url = "https://packages.vmware.com/tools/releases/11.0.1/windows/x64/$package"
 
 Write-Output "***** Downloading VMWare tools"
 Invoke-WebRequest $url -UseBasicParsing -OutFile "C:\Windows\Temp\$package"
